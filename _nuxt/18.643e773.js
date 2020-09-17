58423cbba8294cb7780027e117cf62e9d51433f2
simonknittel <hallo@simonknittel.de>
2020-09-17 17:49:48 +0000

    Deploying to gh-pages from  @ 89a4c6e88148b0c045ba07f9fd9f72f3b3acc2ec 🚀

--- _nuxt/18.643e773.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{198:function(e){e.exports=JSON.parse('{"homepage":{"text":"wordpress.com","url":"https://wordpress.com/"},"hosting":{"text":"Cloud service"},"framework":{"text":"Not applicable"},"database":{"text":"Not applicable"}}')}}]);
