3fb1510dad2d9fdef8083f4e8f4e695c66d952f0
simonknittel <hallo@simonknittel.de>
2021-02-28 12:30:48 +0000

    Deploying to gh-pages from @ simonknittel/cms-comparison@268640ae5a1197ee7e490823e80d4945e68329ef 🚀

--- _nuxt/9c0f66f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{244:function(e,n,t){"use strict";t.r(n);var r={middleware:function(e){return(0,e.redirect)("/content-management-systems")}},c=t(12),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
