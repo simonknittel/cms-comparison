528a36e22010c400e5deb6e241ba43e611dc0dc4
simonknittel <hallo@simonknittel.de>
2021-07-03 15:27:11 +0000

    Deploying to gh-pages from @ simonknittel/cms-comparison@be3d0b3ac05954e08386327235d90fad25c15ab7 🚀

---
+++ _nuxt/5c32670.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{195:function(e){e.exports=JSON.parse('{"homepage":{"text":"wix.com","url":"https://www.wix.com/"},"framework":{"text":"Not applicable"},"hosting":{"text":"SaaS / Cloud service"},"database":{"text":"Not applicable"},"programming_language":{"text":"Not applicable"}}')}}]);
