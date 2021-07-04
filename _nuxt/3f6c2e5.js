85358f97f8a4b4395cb4c644480fc5c455e827b9
simonknittel <hallo@simonknittel.de>
2021-07-04 11:30:38 +0000

    Deploying to gh-pages from @ simonknittel/cms-comparison@00c10955018de18f15f880577d885fa9438519d9 🚀

--- _nuxt/3f6c2e5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{194:function(e){e.exports=JSON.parse('{"homepage":{"text":"squarespace.com","url":"https://www.squarespace.com/"},"framework":{"text":"Not applicable"},"hosting":{"text":"SaaS / Cloud service"},"database":{"text":"Not applicable"},"programming_language":{"text":"Not applicable"}}')}}]);
--- _nuxt/static/1625396236/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcms-comparison\u002F_nuxt\u002Fstatic\u002F1625396236",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fcms-comparison\u002F",assetsPath:"\u002Fcms-comparison\u002F_nuxt\u002F",cdnURL:a}}}}(null));
